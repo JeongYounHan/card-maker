348d678d9db3938661ad5ad4c422be259efbe024
JeongYounHan <51sarang@hanmail.net>
2022-02-27 00:59:35 +0900

    라우터 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,17 @@
 import styles from "./app.module.css";
 import Login from "./components/login/login";
+import Maker from "./components/maker/maker";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App({ authService }) {
   return (
     <div className={styles.app}>
-      <Login authService={authService} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login authService={authService} />}></Route>
+          <Route path="/maker" element={<Maker />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
