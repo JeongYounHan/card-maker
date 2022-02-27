2ab7022a4d4633af6cab62afd5fbaeaa3760d317
JeongYounHan <51sarang@hanmail.net>
2022-02-27 16:23:47 +0900

    maker 페이지 ui 작업

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Editor from "../editor/editor";
@@ -8,6 +9,41 @@ import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "Ellie1",
+      company: "Samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: "",
+    },
+    {
+      id: "2",
+      name: "Ellie2",
+      company: "Samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: "",
+    },
+    {
+      id: "3",
+      name: "Ellie3",
+      company: "Samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: "",
+    },
+  ]);
   const navigate = useNavigate();
   const goToMaker = () => {
     navigate("/");
@@ -26,8 +62,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
