036b073f544b335532d6a3e8f860a67f06390bda
JeongYounHan <51sarang@hanmail.net>
2022-03-01 01:35:04 +0900

    memo로 수정하였으나 작동하지 않음

--- src/components/card_add_form/card_add_form.jsx
+++ src/components/card_add_form/card_add_form.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { memo } from "react";
 import styles from "./card_add_form.module.css";
 import Button from "../button/button";
 // import ImageFileInput from "../image_file_input/image_file_input";
 import { useRef, useState } from "react";
 
-const CardAddForm = ({ FileInput, onAdd }) => {
+const CardAddForm = memo(({ FileInput, onAdd }) => {
   const formRef = useRef();
   const nameRef = useRef();
   const companyRef = useRef();
@@ -58,6 +58,6 @@ const CardAddForm = ({ FileInput, onAdd }) => {
       <Button name="Add" onClick={onSubmit} />
     </form>
   );
-};
+});
 
 export default CardAddForm;
