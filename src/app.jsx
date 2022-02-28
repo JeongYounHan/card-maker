fb621c5962e360ffed52939fccbd19ff25d772fb
JeongYounHan <51sarang@hanmail.net>
2022-02-28 17:49:43 +0900

    firebase 데이터베이스에 저장, 삭제 기능 추가

--- src/app.jsx
+++ src/app.jsx
@@ -3,13 +3,13 @@ import Login from "./components/login/login";
 import Maker from "./components/maker/maker";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App({ FileInput, authService }) {
+function App({ FileInput, authService, cardRepository }) {
   return (
     <div className={styles.app}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login authService={authService} />}></Route>
-          <Route path="/maker" element={<Maker FileInput={FileInput} authService={authService} />}></Route>
+          <Route path="/maker" element={<Maker FileInput={FileInput} authService={authService} cardRepository={cardRepository} />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
