c3c002686e646e1e75caf2c9cd10dab17b4e964a
JeongYounHan <51sarang@hanmail.net>
2022-02-28 14:30:16 +0900

    로딩 스피너 에니메이션 추가

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRef } from "react";
 import styles from "./image_file_input.module.css";
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
   const inputRef = useRef();
+
   const onButtonClick = (event) => {
     event.preventDefault();
     inputRef.current.click();
   };
 
   const onChange = async (event) => {
+    setLoading(true);
     const uploaded = await imageUploader.upload(event.target.files[0]);
+    setLoading(false);
     onFileChange({
       name: uploaded.original_filename,
       url: uploaded.url,
@@ -20,9 +24,12 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
   return (
     <div className={styles.container}>
       <input ref={inputRef} onChange={onChange} className={styles.input} type="file" accept="image/*" name="file" />
-      <button className={styles.button} onClick={onButtonClick}>
-        {name || "No file"}
-      </button>
+      {!loading && (
+        <button className={styles.button} onClick={onButtonClick}>
+          {name || "No file"}
+        </button>
+      )}
+      {loading && <div className={styles.loading}></div>}
     </div>
   );
 };
